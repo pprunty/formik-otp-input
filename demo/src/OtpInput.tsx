7b8bc72e03e9bc1de2e70bb38d213ceb0a8a1f20
pprunty <pprunty@tcd.ie>
2023-11-29 14:03:42 +0000

    updates

--- demo/src/OtpInput.tsx
+++ demo/src/OtpInput.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef, useMemo} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styled from 'styled-components';
 import * as Yup from 'yup';
 
